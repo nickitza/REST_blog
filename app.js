12a5e0b8e0aadab543e090664c57ec91be5c2be4
Nicki Nixon <nickitza@gmail.com>
2019-08-01 13:34:11 -0600

    build new form, new/create CRUD routes

--- app.js
+++ app.js
@@ -40,6 +40,22 @@ app.get("/blogs", function(req, res){
   })
 })
 
+app.get("/blogs/new", function(req, res){
+  res.render("new")
+})
+
+app.post('/blogs', function(req, res){
+  var data = 
+  Blog.create(req.body.blog, function(err, blog){
+    if(err){
+      console.log(err)
+      res.render("new")
+    }else{
+      res.redirect('/blogs')
+    }
+  })
+})
+
 
   app.listen(3000, function(){
     console.log("*** SERVER RUNNING ***")
